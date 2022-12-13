34b729722eeab2b9021198a64118b1050b16fedf
Alex de Landgraaf <alex@maykinmedia.nl>
2022-12-13 14:23:39 +0100

    Adding tests for checking klanten-scopes

--- src/openklant/components/klanten/api/tests/test_auth.py
+++ src/openklant/components/klanten/api/tests/test_auth.py
@@ -3,9 +3,16 @@ Guarantee that the proper authorization machinery is in place.
 """
 from rest_framework.test import APITestCase
 from vng_api_common.tests import AuthCheckMixin, reverse
+from vng_api_common.tests import JWTAuthMixin
+from rest_framework import status
+import requests_mock
 
 from openklant.components.klanten.models.tests.factories import KlantFactory
+from openklant.components.klanten.models.klanten import Klant
+from openklant.components.klanten.api.scopes import SCOPE_KLANTEN_ALLES_LEZEN, SCOPE_KLANTEN_AANMAKEN
+from openklant.components.klanten.models.constants import KlantType, SoortRechtsvorm
 
+SUBJECT = "http://example.com/subject/1"
 
 class KlantScopeForbiddenTests(AuthCheckMixin, APITestCase):
     def test_cannot_create_klant_without_correct_scope(self):
@@ -22,3 +29,47 @@ class KlantScopeForbiddenTests(AuthCheckMixin, APITestCase):
         for url in urls:
             with self.subTest(url=url):
                 self.assertForbidden(url, method="get")
+
+
+class KlantScopeTests(JWTAuthMixin, APITestCase):
+    heeft_alle_autorisaties = False
+    scopes = [SCOPE_KLANTEN_ALLES_LEZEN]
+    maxDiff = None
+
+    def test_list_klanten(self):
+        list_url = reverse(Klant)
+        KlantFactory.create_batch(2)
+        
+        response = self.client.get(list_url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.json()
+        self.assertEqual(len(data["results"]), 2)
+
+    def test_create_klant_url(self):
+        list_url = reverse(Klant)
+        data = {
+            "bronorganisatie": "950428139",
+            "klantnummer": "1111",
+            "websiteUrl": "http://some.website.com",
+            "voornaam": "Xavier",
+            "achternaam": "Jackson",
+            "emailadres": "test@gmail.com",
+            "adres": {
+                "straatnaam": "Keizersgracht",
+                "huisnummer": "117",
+                "huisletter": "A",
+                "postcode": "1015CJ",
+                "woonplaatsnaam": "test",
+                "landcode": "1234",
+            },
+            "subjectType": KlantType.natuurlijk_persoon,
+            "subject": SUBJECT,
+        }
+
+        with requests_mock.Mocker() as m:
+            m.get(SUBJECT, json={})
+            response = self.client.post(list_url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
