c74262bab3908f9e15364fb2f62a1a2e02da7fe2
Alex de Landgraaf <alex@maykinmedia.nl>
2023-07-24 17:43:20 +0200

    Adding missing envvars for docker-compose

--- docker-init-db.sql
+++ docker-init-db.sql
@@ -1,4 +1,4 @@
-CREATE USER {{ project_name|lower }};
-CREATE DATABASE {{ project_name|lower }};
-GRANT ALL PRIVILEGES ON DATABASE {{ project_name|lower }} TO {{ project_name|lower }};
+CREATE USER openklant;
+CREATE DATABASE openklant;
+GRANT ALL PRIVILEGES ON DATABASE openklant TO openklant;
 
