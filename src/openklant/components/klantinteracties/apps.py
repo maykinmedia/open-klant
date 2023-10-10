c4c826524878d12920cd03d9e31e92ea548a4cc6
bart-maykin <bart.burgman@maykinmedia.nl>
2023-10-10 17:58:32 +0200

    :card_file_box: [#61] created Partij, Persoon, ContactPersoon and Organisatie  models

---
+++ src/openklant/components/klantinteracties/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KlantinteractiesConfig(AppConfig):
+    name = "openklant.components.klantinteracties"
---
+++ src/openklant/components/klantinteracties/constants.py
@@ -0,0 +1,60 @@
+from django.utils.translation import gettext_lazy as _
+
+from djchoices import ChoiceItem, DjangoChoices
+
+
+# TODO: change value when document gets updated:
+# https://vng-realisatie.github.io/klantinteracties/informatiemodel/gegevenswoordenboek#detail_class_Model_Initiator
+class Initiator(DjangoChoices):
+    gemeente = ChoiceItem("gemeente", _("Gemeente"))
+    klant = ChoiceItem("klant", _("Klant"))
+    vertegenwoordiger = ChoiceItem("vertegenwoordiger", _("Vertegenwoordiger"))
+
+
+class Taakstatus(DjangoChoices):
+    te_verwerken = ChoiceItem("te_verwerken", _("Het verzoek is afgehandeld."))
+    verwerkt = ChoiceItem("verwerkt", _("Het verzoek id buiten behandeling gesteld."))
+
+
+class SoortBezoekadres(DjangoChoices):
+    binnenlands_adres = ChoiceItem("binnenlands_adres", _("Binnenlands adres"))
+    buitenlands_adres = ChoiceItem("binnenlands_adres", _("Buitenlands adres"))
+
+
+class AanduidingBijHuisnummer(DjangoChoices):
+    bij = ChoiceItem("bij", _("Bij"))
+    tegenover = ChoiceItem("tegenover", _("Tegenover"))
+
+
+class SoortCorrespondentieadres(DjangoChoices):
+    postbusnummer = ChoiceItem("postbusnummer", _("Postbusnummer"))
+    antwoordnummer = ChoiceItem("antwoordnummer", _("Antwoordnummer"))
+    binnenlands_adres = ChoiceItem("binnenlands_adres", _("Binnenlands adres"))
+    buitenlands_adres = ChoiceItem("buitenlands_adres", _("Buitenlands adres"))
+
+
+class SoortActor(DjangoChoices):
+    medewerker = ChoiceItem("medewerker", _("Medewerker"))
+    geautomatiseerde_actor = ChoiceItem(
+        "geautomatiseerde_actor", _("Geautomatiseerde actor")
+    )
+    organisatorische_eenheid = ChoiceItem(
+        "organisatorische_eenheid", _("Organisatorische eenheid")
+    )
+
+
+class SoortInhoudsdeel(DjangoChoices):
+    informatieobject = ChoiceItem("informatieobject", _("Informatieobject"))
+    overig_object = ChoiceItem("overig_object", _("Overig object"))
+    tekst = ChoiceItem("tekst", _("Tekst"))
+
+
+class SoortPartij(DjangoChoices):
+    persoon = ChoiceItem("persoon", _("Persoon"))
+    organisatie = ChoiceItem("organisatie", _("Organisatie"))
+    contactpersoon = ChoiceItem("contactpersoon", _("Contactpersoon"))
+
+
+class Klantcontrol(DjangoChoices):
+    vertegenwoordiger = ChoiceItem("vertegenwoordiger", _("Vertegenwoordiger"))
+    klant = ChoiceItem("klant", _("Klant"))
