65fc76685e352fe8356f0a6ba88a12057344b250
bart-maykin <bart.burgman@maykinmedia.nl>
2023-10-17 12:43:16 +0200

    :art: [#66] changed gegevensgroeptypes into model mixins

--- src/openklant/components/klantinteracties/models/mixins.py
+++ src/openklant/components/klantinteracties/models/mixins.py
@@ -1,7 +1,180 @@
+from django.core.validators import MinLengthValidator, validate_integer
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from vng_api_common.descriptors import GegevensGroepType
+
+
+class BezoekAdresMixin(models.Model):
+    bezoekadres_nummeraanduiding_id = models.UUIDField(
+        verbose_name=_("Nummeraanduiding ID"),
+        help_text=_(
+            "Identificatie van het adres bij de Basisregistratie Adressen en Gebouwen."
+        ),
+        unique=True,
+        blank=True,
+        null=True,
+    )
+    bezoekadres_adresregel1 = models.CharField(
+        _("Adresregel 1"),
+        help_text=_(
+            "Eerste deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
+        ),
+        max_length=80,
+        blank=True,
+    )
+    bezoekadres_adresregel2 = models.CharField(
+        _("Adresregel 2"),
+        help_text=_(
+            "Tweede deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
+        ),
+        max_length=80,
+        blank=True,
+    )
+    bezoekadres_adresregel3 = models.CharField(
+        _("Adresregel 3"),
+        help_text=_(
+            "Derde deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
+        ),
+        max_length=80,
+        blank=True,
+    )
+    bezoekadres_land = models.CharField(
+        _("Land"),
+        help_text=_(
+            "Een code, opgenomen in Tabel 34, Landentabel, die het land (buiten Nederland) "
+            "aangeeft alwaar de ingeschrevene verblijft."
+        ),
+        validators=[
+            MinLengthValidator(limit_value=4),
+            validate_integer,
+        ],
+        max_length=4,
+        blank=True,
+    )
+
+    bezoekadres = GegevensGroepType(
+        {
+            "nummeraanduiding id": bezoekadres_nummeraanduiding_id,
+            "adresregel 1": bezoekadres_adresregel1,
+            "adresregel 2": bezoekadres_adresregel2,
+            "adresregel 3": bezoekadres_adresregel3,
+            "land": bezoekadres_land,
+        }
+    )
+
+    class Meta:
+        abstract = True
+
+
+class CorrespondentieAdresMixin(models.Model):
+    correspondentieadres_nummeraanduiding_id = models.UUIDField(
+        verbose_name=_("Nummeraanduiding ID"),
+        help_text=_(
+            "Identificatie van het adres bij de Basisregistratie Adressen en Gebouwen."
+        ),
+        unique=True,
+        blank=True,
+        null=True,
+    )
+    correspondentieadres_adresregel1 = models.CharField(
+        _("Adresregel 1"),
+        help_text=_(
+            "Eerste deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
+        ),
+        max_length=80,
+        blank=True,
+    )
+    correspondentieadres_adresregel2 = models.CharField(
+        _("Adresregel 2"),
+        help_text=_(
+            "Tweede deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
+        ),
+        max_length=80,
+        blank=True,
+    )
+    correspondentieadres_adresregel3 = models.CharField(
+        _("Adresregel 3"),
+        help_text=_(
+            "Derde deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
+        ),
+        max_length=80,
+        blank=True,
+    )
+    correspondentieadres_land = models.CharField(
+        _("Land"),
+        help_text=_(
+            "Een code, opgenomen in Tabel 34, Landentabel, die het land (buiten Nederland) "
+            "aangeeft alwaar de ingeschrevene verblijft."
+        ),
+        validators=[
+            MinLengthValidator(limit_value=4),
+            validate_integer,
+        ],
+        max_length=4,
+        blank=True,
+    )
+
+    correspondentieadres = GegevensGroepType(
+        {
+            "nummeraanduiding id": correspondentieadres_nummeraanduiding_id,
+            "adresregel 1": correspondentieadres_adresregel1,
+            "adresregel 2": correspondentieadres_adresregel2,
+            "adresregel 3": correspondentieadres_adresregel3,
+            "land": correspondentieadres_land,
+        },
+    )
+
+    class Meta:
+        abstract = True
+
+
+class ContactNaamMixin(models.Model):
+    contactnaam_voorletters = models.CharField(
+        _("Voorletters"),
+        help_text=_(
+            "Een afkorting van de voornamen. Meestal de beginletter, maar in sommige gevallen "
+            "de beginletter gecombineerd met de tweede letter van een voornaam."
+        ),
+        max_length=10,
+    )
+    contactnaam_voornaam = models.CharField(
+        _("Voornaam"),
+        help_text=_(
+            "De voornaam die de persoon wil gebruiken tijdens communicatie met de gemeente."
+        ),
+        max_length=200,
+        blank=True,
+    )
+    contactnaam_voorvoegsel_achternaam = models.CharField(
+        _("Voorvoegsel achternaam"),
+        help_text=_(
+            "Een eventueel voorvoegsel dat hoort bij de achternaam die de persoon "
+            "wil gebruiken tijdens communicatie met de gemeente."
+        ),
+        max_length=10,
+        blank=True,
+    )
+    contactnaam_achternaam = models.CharField(
+        _("Achternaam"),
+        help_text=_(
+            "Een achternaam die de persoon wil gebruiken tijdens communicatie met de gemeente."
+        ),
+        max_length=200,
+        blank=True,
+    )
+
+    Contactnaam = GegevensGroepType(
+        {
+            "voorletters": contactnaam_voorletters,
+            "voornaam": contactnaam_voornaam,
+            "voorvoegsel achternaam": contactnaam_voorvoegsel_achternaam,
+            "achternaam": contactnaam_achternaam,
+        }
+    )
+
+    class Meta:
+        abstract = True
 
 
 class ActorIdentifcatorMixin(models.Model):
--- src/openklant/components/klantinteracties/models/partijen.py
+++ src/openklant/components/klantinteracties/models/partijen.py
@@ -1,17 +1,16 @@
 import uuid
 
-from django.core.validators import MinLengthValidator, validate_integer
+from django.core.validators import validate_integer
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-from vng_api_common.descriptors import GegevensGroepType
 
 from .constants import SoortPartij
 from .digitaal_adres import DigitaalAdres
 from .klantcontacten import Betrokkene
+from .mixins import BezoekAdresMixin, ContactNaamMixin, CorrespondentieAdresMixin
 
 
-class Partij(models.Model):
+class Partij(BezoekAdresMixin, CorrespondentieAdresMixin):
     id = models.UUIDField(
         primary_key=True,
         unique=True,
@@ -88,122 +87,6 @@ class Partij(models.Model):
         ),
     )
 
-    # Correspondentieadres model fields:
-    correspondentieadres_nummeraanduiding_id = models.UUIDField(
-        verbose_name=_("Nummeraanduiding ID"),
-        help_text=_(
-            "Identificatie van het adres bij de Basisregistratie Adressen en Gebouwen."
-        ),
-        unique=True,
-        blank=True,
-        null=True,
-    )
-    correspondentieadres_adresregel1 = models.CharField(
-        _("Adresregel 1"),
-        help_text=_(
-            "Eerste deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
-        ),
-        max_length=80,
-        blank=True,
-    )
-    correspondentieadres_adresregel2 = models.CharField(
-        _("Adresregel 2"),
-        help_text=_(
-            "Tweede deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
-        ),
-        max_length=80,
-        blank=True,
-    )
-    correspondentieadres_adresregel3 = models.CharField(
-        _("Adresregel 3"),
-        help_text=_(
-            "Derde deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
-        ),
-        max_length=80,
-        blank=True,
-    )
-    correspondentieadres_land = models.CharField(
-        _("Land"),
-        help_text=_(
-            "Een code, opgenomen in Tabel 34, Landentabel, die het land (buiten Nederland) "
-            "aangeeft alwaar de ingeschrevene verblijft."
-        ),
-        validators=[
-            MinLengthValidator(limit_value=4),
-            validate_integer,
-        ],
-        max_length=4,
-        blank=True,
-    )
-
-    # Bezoekadres model fields:
-    bezoekadres_nummeraanduiding_id = models.UUIDField(
-        verbose_name=_("Nummeraanduiding ID"),
-        help_text=_(
-            "Identificatie van het adres bij de Basisregistratie Adressen en Gebouwen."
-        ),
-        unique=True,
-        blank=True,
-        null=True,
-    )
-    bezoekadres_adresregel1 = models.CharField(
-        _("Adresregel 1"),
-        help_text=_(
-            "Eerste deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
-        ),
-        max_length=80,
-        blank=True,
-    )
-    bezoekadres_adresregel2 = models.CharField(
-        _("Adresregel 2"),
-        help_text=_(
-            "Tweede deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
-        ),
-        max_length=80,
-        blank=True,
-    )
-    bezoekadres_adresregel3 = models.CharField(
-        _("Adresregel 3"),
-        help_text=_(
-            "Derde deel van het adres dat niet voorkomt in de Basisregistratie Adressen en Gebouwen."
-        ),
-        max_length=80,
-        blank=True,
-    )
-    bezoekadres_land = models.CharField(
-        _("Land"),
-        help_text=_(
-            "Een code, opgenomen in Tabel 34, Landentabel, die het land (buiten Nederland) "
-            "aangeeft alwaar de ingeschrevene verblijft."
-        ),
-        validators=[
-            MinLengthValidator(limit_value=4),
-            validate_integer,
-        ],
-        max_length=4,
-        blank=True,
-    )
-
-    # Group types:
-    correspondentieadres = GegevensGroepType(
-        {
-            "nummeraanduiding id": correspondentieadres_nummeraanduiding_id,
-            "adresregel 1": correspondentieadres_adresregel1,
-            "adresregel 2": correspondentieadres_adresregel2,
-            "adresregel 3": correspondentieadres_adresregel3,
-            "land": correspondentieadres_land,
-        },
-    )
-    bezoekadres = GegevensGroepType(
-        {
-            "nummeraanduiding id": bezoekadres_nummeraanduiding_id,
-            "adresregel 1": bezoekadres_adresregel1,
-            "adresregel 2": bezoekadres_adresregel2,
-            "adresregel 3": bezoekadres_adresregel3,
-            "land": bezoekadres_land,
-        }
-    )
-
     class Meta:
         verbose_name = _("partij")
         verbose_name_plural = _("partijen")
@@ -232,58 +115,13 @@ class Organisatie(models.Model):
             return self.naam
 
 
-class Persoon(models.Model):
+class Persoon(ContactNaamMixin):
     partij = models.ForeignKey(
         Partij,
         on_delete=models.CASCADE,
         verbose_name=_("Partij"),
         related_name="persoon",
         null=True,
-    )
-
-    # Contactnaam model fields:
-    contactnaam_voorletters = models.CharField(
-        _("Voorletters"),
-        help_text=_(
-            "Een afkorting van de voornamen. Meestal de beginletter, maar in sommige gevallen "
-            "de beginletter gecombineerd met de tweede letter van een voornaam."
-        ),
-        max_length=10,
-    )
-    contactnaam_voornaam = models.CharField(
-        _("Voornaam"),
-        help_text=_(
-            "De voornaam die de persoon wil gebruiken tijdens communicatie met de gemeente."
-        ),
-        max_length=200,
-        blank=True,
-    )
-    contactnaam_voorvoegsel_achternaam = models.CharField(
-        _("Voorvoegsel achternaam"),
-        help_text=_(
-            "Een eventueel voorvoegsel dat hoort bij de achternaam die de persoon "
-            "wil gebruiken tijdens communicatie met de gemeente."
-        ),
-        max_length=10,
-        blank=True,
-    )
-    contactnaam_achternaam = models.CharField(
-        _("Achternaam"),
-        help_text=_(
-            "Een achternaam die de persoon wil gebruiken tijdens communicatie met de gemeente."
-        ),
-        max_length=200,
-        blank=True,
-    )
-
-    # Group types:
-    Contactnaam = GegevensGroepType(
-        {
-            "voorletters": contactnaam_voorletters,
-            "voornaam": contactnaam_voornaam,
-            "voorvoegsel achternaam": contactnaam_voorvoegsel_achternaam,
-            "achternaam": contactnaam_achternaam,
-        }
     )
 
     class Meta:
@@ -294,7 +132,7 @@ class Persoon(models.Model):
             return self.contactnaam_voorletters
 
 
-class Contactpersoon(models.Model):
+class Contactpersoon(ContactNaamMixin):
     partij = models.ForeignKey(
         Partij,
         on_delete=models.CASCADE,
@@ -309,51 +147,6 @@ class Contactpersoon(models.Model):
         related_name="contact_personen",
         help_text=_("De organisatie waar een contactpersoon voor werkt."),
         null=True,
-    )
-
-    # Contactnaam model fields:
-    contactnaam_voorletters = models.CharField(
-        _("Voorletters"),
-        help_text=_(
-            "Een afkorting van de voornamen. Meestal de beginletter, maar in sommige gevallen "
-            "de beginletter gecombineerd met de tweede letter van een voornaam."
-        ),
-        max_length=10,
-    )
-    contactnaam_voornaam = models.CharField(
-        _("Voornaam"),
-        help_text=_(
-            "De voornaam die de persoon wil gebruiken tijdens communicatie met de gemeente."
-        ),
-        max_length=200,
-        blank=True,
-    )
-    contactnaam_voorvoegsel_achternaam = models.CharField(
-        _("Voorvoegsel achternaam"),
-        help_text=_(
-            "Een eventueel voorvoegsel dat hoort bij de achternaam die de persoon "
-            "wil gebruiken tijdens communicatie met de gemeente."
-        ),
-        max_length=10,
-        blank=True,
-    )
-    contactnaam_achternaam = models.CharField(
-        _("Achternaam"),
-        help_text=_(
-            "Een achternaam die de persoon wil gebruiken tijdens communicatie met de gemeente."
-        ),
-        max_length=200,
-        blank=True,
-    )
-
-    # Group types:
-    Contactnaam = GegevensGroepType(
-        {
-            "voorletters": contactnaam_voorletters,
-            "voornaam": contactnaam_voornaam,
-            "voorvoegsel achternaam": contactnaam_voorvoegsel_achternaam,
-            "achternaam": contactnaam_achternaam,
-        }
     )
 
     class Meta:
