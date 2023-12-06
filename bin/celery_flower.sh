adc085a7b76e1f5a941b46afb0161dd8b909f392
Joeri Bekker <joeri@maykinmedia.nl>
2023-12-06 17:35:23 +0100

    :fire: Cleanup

--- bin/celery_flower.sh
+++ bin/celery_flower.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+set -e
+
 exec celery flower --app openklant --workdir src
--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -13,23 +13,45 @@ uwsgi_port=${UWSGI_PORT:-8000}
 uwsgi_processes=${UWSGI_PROCESSES:-4}
 uwsgi_threads=${UWSGI_THREADS:-1}
 
-until pg_isready; do
-  >&2 echo "Waiting for database connection..."
-  sleep 1
-done
+mountpoint=${SUBPATH:-/}
 
->&2 echo "Database is up."
+# wait for required services
+${SCRIPTPATH}/wait_for_db.sh
 
 # Apply database migrations
 >&2 echo "Apply database migrations"
 python src/manage.py migrate
+
+# Load any JSON fixtures present
+if [ -d $fixtures_dir ]; then
+    echo "Loading fixtures from $fixtures_dir"
+
+    for fixture in $(ls "$fixtures_dir/"*.json)
+    do
+        echo "Loading fixture $fixture"
+        python src/manage.py loaddata $fixture
+    done
+fi
+
+# Create superuser
+# specify password by setting DJANGO_SUPERUSER_PASSWORD in the env
+# specify username by setting OPENZAAK_SUPERUSER_USERNAME in the env
+# specify email by setting OPENZAAK_SUPERUSER_EMAIL in the env
+if [ -n "${OPENZAAK_SUPERUSER_USERNAME}" ]; then
+    python src/manage.py createinitialsuperuser \
+        --no-input \
+        --username "${OPENZAAK_SUPERUSER_USERNAME}" \
+        --email "${OPENZAAK_SUPERUSER_EMAIL:-admin\@admin.org}"
+    unset OPENZAAK_SUPERUSER_USERNAME OPENZAAK_SUPERUSER_EMAIL DJANGO_SUPERUSER_PASSWORD
+fi
 
 # Start server
 >&2 echo "Starting server"
 exec uwsgi \
     --http :$uwsgi_port \
     --http-keepalive \
-    --module openklant.wsgi \
+    --manage-script-name \
+    --mount $mountpoint=openklant.wsgi:application \
     --static-map /static=/app/static \
     --static-map /media=/app/media  \
     --chdir src \
