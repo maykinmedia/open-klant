d8456d99a067ffeb1bbb4a9c048fffe514e8d6f7
Joeri Bekker <joeri@maykinmedia.nl>
2024-03-15 18:22:39 +0100

    :wrench: Added script to export demo data.

---
+++ bin/generate_demo_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Dump the current data into a fixtures, intended for demo data.
+#
+# You can load this fixture with:
+# $ src/manage.py loaddata src/openklant/fixtures/<file>
+#
+# Run this script from the root of the repository
+
+src/manage.py dumpdata --indent=4 --natural-foreign --natural-primary klantinteracties > src/openklant/fixtures/klantinteracties.json
+src/manage.py dumpdata --indent=4 --natural-foreign --natural-primary contactgegevens > src/openklant/fixtures/contactgegevens.json
