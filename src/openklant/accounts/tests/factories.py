6270c88693e1025da8b75b9f74ffba2f2beb2f1f
Conor Holden <conor@maykinmedia.nl>
2024-06-17 11:59:33 +0200

    :white_check_mark:[#189] add 2fa test

--- src/openklant/accounts/tests/factories.py
+++ src/openklant/accounts/tests/factories.py
@@ -1,4 +1,14 @@
 import factory
+from django_otp.plugins.otp_static.models import StaticDevice, StaticToken
+from django_otp.util import random_hex
+
+
+class TOTPDeviceFactory(factory.django.DjangoModelFactory):
+    user = factory.SubFactory("nrc.accounts.tests.factories.UserFactory")
+    key = factory.LazyAttribute(lambda o: random_hex())
+
+    class Meta:
+        model = "otp_totp.TOTPDevice"
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -8,7 +18,32 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = "accounts.User"
 
+    class Params:
+        with_totp_device = factory.Trait(
+            device=factory.RelatedFactory(
+                TOTPDeviceFactory,
+                "user",
+                name="default",
+            )
+        )
+
 
 class SuperUserFactory(UserFactory):
     is_staff = True
     is_superuser = True
+
+
+class RecoveryDeviceFactory(factory.django.DjangoModelFactory):
+    user = factory.SubFactory("nrc.accounts.tests.factories.UserFactory")
+    name = "backup"
+
+    class Meta:
+        model = StaticDevice
+
+
+class RecoveryTokenFactory(factory.django.DjangoModelFactory):
+    device = factory.SubFactory(RecoveryDeviceFactory)
+    token = factory.LazyFunction(StaticToken.random_token)
+
+    class Meta:
+        model = StaticToken
