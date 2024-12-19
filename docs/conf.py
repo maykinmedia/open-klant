a3537ff1032a53ab7be187e7d6620c6322acc84e
Steven Bal <steven@maykinmedia.nl>
2024-12-19 16:03:46 +0100

    :construction_worker: [maykinmedia/open-api-framework#92] Make sure docs are built in CI

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ intersphinx_mapping = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_logo = "logo.svg"
+# html_logo = "logo.svg"
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
