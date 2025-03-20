d5671ff210d9c63e16dd9ea7f5263b34782ad817
Steven Bal <steven@maykinmedia.nl>
2025-03-20 15:48:53 +0100

    :bookmark: Release version 2.6.1

--- src/openklant/__init__.py
+++ src/openklant/__init__.py
@@ -1,6 +1,6 @@
 from .celery import app as celery_app
 
 __all__ = ("celery_app",)
-__version__ = "2.6.0"
+__version__ = "2.6.1"
 __author__ = "Maykin"
 __homepage__ = "https://github.com/maykinmedia/open-klant"
