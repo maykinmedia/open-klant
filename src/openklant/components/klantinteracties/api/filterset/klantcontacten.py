55a6af24ecd1c2fa5ce33294ae616b0a6ae03e56
Floris272 <floris@maykinmedia.nl>
2025-04-15 11:08:07 +0200

    :sparkles: [#342] add betrokkene & digitale adressen partij identificator filters

--- src/openklant/components/klantinteracties/api/filterset/klantcontacten.py
+++ src/openklant/components/klantinteracties/api/filterset/klantcontacten.py
@@ -117,7 +117,9 @@ class KlantcontactFilterSet(FilterSet):
         self, queryset, name, value
     ):
         try:
-            return queryset.filter(betrokkene__partij__partijidentificator__partij_identificator_code_objecttype=value)
+            return queryset.filter(
+                betrokkene__partij__partijidentificator__partij_identificator_code_objecttype=value
+            )
         except ValueError:
             return queryset.none()
 
@@ -192,6 +194,30 @@ class BetrokkeneFilterSet(FilterSet):
         help_text=_("Zoek betrokkene object op basis van het partij uuid"),
         field_name="partij__uuid",
     )
+    was_partij__partij_identificator__code_objecttype = filters.CharFilter(
+        help_text=_(
+            "Zoek betrokkene object op basis van het partij identificator objecttype."
+        ),
+        method="filter_partij_identificator_code_objecttype",
+    )
+    was_partij__partij_identificator__code_soort_object_id = filters.CharFilter(
+        help_text=_(
+            "Zoek betrokkene object op basis van het partij identificator soort object ID."
+        ),
+        method="filter_partij_identificator_code_soort_object_id",
+    )
+    was_partij__partij_identificator__object_id = filters.CharFilter(
+        help_text=_(
+            "Zoek betrokkene object op basis van het partij identificator object ID."
+        ),
+        method="filter_partij_identificator_object_id",
+    )
+    was_partij__partij_identificator__code_register = filters.CharFilter(
+        help_text=_(
+            "Zoek betrokkene object op basis van het partij identificator register."
+        ),
+        method="filter_partij_identificator_code_register",
+    )
     expand = ExpandFilter(serializer_class=BetrokkeneSerializer)
 
     class Meta:
@@ -210,6 +236,10 @@ class BetrokkeneFilterSet(FilterSet):
             "was_partij__nummer",
             "was_partij__url",
             "was_partij__uuid",
+            "was_partij__partij_identificator__code_objecttype",
+            "was_partij__partij_identificator__code_soort_object_id",
+            "was_partij__partij_identificator__object_id",
+            "was_partij__partij_identificator__code_register",
             "organisatienaam",
         )
 
@@ -224,6 +254,38 @@ class BetrokkeneFilterSet(FilterSet):
 
     def filter_partij_nummer(self, queryset, name, value):
         return queryset.filter(partij__nummer=value)
+
+    def filter_partij_identificator_code_objecttype(self, queryset, name, value):
+        try:
+            return queryset.filter(
+                partij__partijidentificator__partij_identificator_code_objecttype=value
+            )
+        except ValueError:
+            return queryset.none()
+
+    def filter_partij_identificator_code_soort_object_id(self, queryset, name, value):
+        try:
+            return queryset.filter(
+                partij__partijidentificator__partij_identificator_code_soort_object_id=value
+            )
+        except ValueError:
+            return queryset.none()
+
+    def filter_partij_identificator_object_id(self, queryset, name, value):
+        try:
+            return queryset.filter(
+                partij__partijidentificator__partij_identificator_object_id=value
+            )
+        except ValueError:
+            return queryset.none()
+
+    def filter_partij_identificator_code_register(self, queryset, name, value):
+        try:
+            return queryset.filter(
+                partij__partijidentificator__partij_identificator_code_register=value
+            )
+        except ValueError:
+            return queryset.none()
 
 
 class ActorKlantcontactFilterSet(FilterSet):
