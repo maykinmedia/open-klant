11911d39d6bcbb078c8b6acef027c94799211893
Tim de Beer <tim@maykinmedia.nl>
2025-06-12 12:24:17 +0200

    🔧 [#434] Configure structlog for celery

--- src/openklant/celery.py
+++ src/openklant/celery.py
@@ -1,4 +1,14 @@
+import logging  # noqa: TID251 - correct use to replace stdlib logging
+import logging.config  # noqa: TID251 - correct use to replace stdlib logging
+from pathlib import Path
+
+from django.conf import settings
+
+import structlog
 from celery import Celery
+from celery.signals import worker_ready, worker_shutdown
+from django_structlog.celery.steps import DjangoStructLogInitStep
+from open_api_framework.conf.utils import config
 
 from openklant.setup import setup_env
 
@@ -6,5 +16,133 @@ setup_env()
 
 app = Celery("openklant")
 
+assert app.steps is not None
+app.steps["worker"].add(DjangoStructLogInitStep)
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.ONCE = {
+    "backend": "celery_once.backends.Redis",
+    "settings": {
+        "url": settings.CELERY_BROKER_URL,
+        "default_timeout": 60 * 60,  # one hour
+    },
+}
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+
+def receiver_setup_logging(
+    loglevel, logfile, format, colorize, **kwargs
+):  # pragma: no cover
+    formatter = config("LOG_FORMAT_CONSOLE", default="json")
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "json": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processor": structlog.processors.JSONRenderer(),
+                    "foreign_pre_chain": [
+                        structlog.contextvars.merge_contextvars,
+                        structlog.processors.TimeStamper(fmt="iso"),
+                        structlog.stdlib.add_logger_name,
+                        structlog.stdlib.add_log_level,
+                        structlog.stdlib.PositionalArgumentsFormatter(),
+                    ],
+                },
+                "plain_console": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processor": structlog.dev.ConsoleRenderer(),
+                    "foreign_pre_chain": [
+                        structlog.contextvars.merge_contextvars,
+                        structlog.processors.TimeStamper(fmt="iso"),
+                        structlog.stdlib.add_logger_name,
+                        structlog.stdlib.add_log_level,
+                        structlog.stdlib.PositionalArgumentsFormatter(),
+                    ],
+                },
+            },
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                    "formatter": formatter,
+                },
+            },
+            "loggers": {
+                "root": {
+                    "handlers": ["console"],
+                    "level": "INFO",
+                },
+                "objects": {
+                    "handlers": ["console"],
+                    "level": "INFO",
+                },
+                "django_structlog": {
+                    "handlers": ["console"],
+                    "level": "INFO",
+                },
+            },
+        }
+    )
+
+    exception_processors = (
+        [structlog.processors.format_exc_info] if formatter == "json" else []
+    )
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.stdlib.filter_by_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.StackInfoRenderer(),
+            *exception_processors,
+            structlog.processors.UnicodeDecoder(),
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        ],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
+
+
+HEARTBEAT_FILE = Path(settings.BASE_DIR) / "tmp" / "celery_worker_heartbeat"
+READINESS_FILE = Path(settings.BASE_DIR) / "tmp" / "celery_worker_ready"
+
+
+#
+# Utilities for checking the health of celery workers
+#
+class LivenessProbe(bootsteps.StartStopStep):
+    requires = {"celery.worker.components:Timer"}
+
+    def __init__(self, worker, **kwargs):
+        self.requests = []
+        self.tref = None
+
+    def start(self, worker):
+        self.tref = worker.timer.call_repeatedly(
+            10.0,
+            self.update_heartbeat_file,
+            (worker,),
+            priority=10,
+        )
+
+    def stop(self, worker):
+        HEARTBEAT_FILE.unlink(missing_ok=True)
+
+    def update_heartbeat_file(self, worker):
+        HEARTBEAT_FILE.touch()
+
+
+@worker_ready.connect
+def worker_ready(**_):
+    READINESS_FILE.touch()
+
+
+@worker_shutdown.connect
+def worker_shutdown(**_):
+    READINESS_FILE.unlink(missing_ok=True)
+
+
+app.steps["worker"].add(LivenessProbe)
