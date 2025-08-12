6093d63d343346ac98742fbc60a591c31d20e88a
Daniel Mursa <daniel@maykinmedia.nl>
2025-08-12 14:09:43 +0200

    :fire: Replace IPThrottleMixin

--- src/openklant/accounts/views/password_reset.py
+++ src/openklant/accounts/views/password_reset.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import views as auth_views
 
-from openklant.utils.mixins import IPThrottleMixin
+from maykin_common.throttling import IPThrottleMixin
 
 
 class PasswordResetView(IPThrottleMixin, auth_views.PasswordResetView):
     throttle_name = "password-reset"
     throttle_visits = 5
     throttle_period = 60
-    throttle_methods = ["get"]
+    throttle_methods = ("get",)
