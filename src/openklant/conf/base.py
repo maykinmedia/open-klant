68ecf165f3d79f3b02046a88e11842727c287b81
Daniel Mursa <daniel@maykinmedia.nl>
2025-08-12 12:39:03 +0200

    :construction: Fix settings and remove obsolete files

--- src/openklant/conf/base.py
+++ src/openklant/conf/base.py
@@ -13,6 +13,7 @@ from .api import *  # noqa
 # APPLICATIONS enabled for this project
 #
 INSTALLED_APPS = INSTALLED_APPS + [
+    "maykin_common",
     # External applications.
     "vng_api_common.notifications",
     # Project applications.
@@ -49,7 +50,9 @@ SITE_TITLE = "API dashboard"
 # Django-Admin-Index
 #
 ADMIN_INDEX_SHOW_REMAINING_APPS_TO_SUPERUSERS = True
-
+ADMIN_INDEX_DISPLAY_DROP_DOWN_MENU_CONDITION_FUNCTION = (
+    "maykin_common.django_two_factor_auth.should_display_dropdown_menu"
+)
 #
 # Django setup configuration
 #
