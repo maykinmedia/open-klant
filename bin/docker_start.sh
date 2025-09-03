8a5b5cc083f6d32d052adee7ac2f412995f3e90f
Daniel Mursa <daniel@maykinmedia.nl>
2025-09-03 15:58:28 +0200

    :hammer: Apply some best practices to uwsgi
    OpenForms commit https://github.com/open-formulieren/open-forms/pull/5452/commits/c6494cf1753140c64bf56287de3f0b3ed9f090c2

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -32,6 +32,7 @@ python src/manage.py migrate
 # Start server
 >&2 echo "Starting server"
 uwsgi \
+    --strict \
     --ini "${SCRIPTPATH}/uwsgi.ini" \
     --http :$uwsgi_port \
     --http-keepalive \
@@ -41,6 +42,9 @@ uwsgi \
     --static-map /media=/app/media  \
     --chdir src \
     --enable-threads \
+    --single-interpreter \
+    --die-on-term \
+    --need-app \
     --processes $uwsgi_processes \
     --threads $uwsgi_threads \
     --post-buffering=8192 \
