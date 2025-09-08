04c06baa972a2be198e796242f743317d5f5637e
Daniel Mursa <daniel@maykinmedia.nl>
2025-09-08 12:09:31 +0200

    :wrench: Enable OpenTelemetry integration

--- src/openklant/conf/dev.py
+++ src/openklant/conf/dev.py
@@ -20,6 +20,9 @@ os.environ.setdefault("LOG_FORMAT_CONSOLE", "plain_console")
 os.environ.setdefault("RELEASE", "dev")
 os.environ.setdefault("LOG_REQUESTS", "no")
 
+os.environ.setdefault("OTEL_SDK_DISABLED", "true")
+os.environ.setdefault("OTEL_EXPORTER_OTLP_METRICS_INSECURE", "true")
+
 from .base import *  # noqa isort:skip
 
 # Feel free to switch dev to sqlite3 for simple projects,
