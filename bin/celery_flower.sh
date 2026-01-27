2e3d8fc722922519122f3b4b89ad1506e3389be2
Daniel Mursa <daniel@maykinmedia.nl>
2026-01-27 10:56:42 +0100

    :recycle: [#422] Restructure celery module and add test_celery.py

--- bin/celery_flower.sh
+++ bin/celery_flower.sh
@@ -3,4 +3,4 @@
 # Set defaults for OTEL
 export OTEL_SERVICE_NAME="${OTEL_SERVICE_NAME:-openklant-flower}"
 
-exec celery flower --app openklant --workdir src
+exec celery --workdir src -A openklant.celery flower
