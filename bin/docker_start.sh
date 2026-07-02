010fa952cad61ff8cf0a5599b75508fb6244d086
Daniel Mursa <daniel@maykinmedia.nl>
2026-07-02 10:13:48 +0200

    :construction_worker: Fix uwsgi strict-mode crash by renaming UWSGI_PORT to OPENKLANT_PORT

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -9,7 +9,7 @@ export PGPORT=${DB_PORT:-5432}
 
 fixtures_dir=${FIXTURES_DIR:-/app/fixtures}
 
-uwsgi_port=${UWSGI_PORT:-8000}
+uwsgi_port=${OPENKLANT_PORT:-8000}
 uwsgi_processes=${UWSGI_PROCESSES:-4}
 uwsgi_threads=${UWSGI_THREADS:-4}
 
